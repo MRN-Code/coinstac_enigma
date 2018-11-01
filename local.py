bfe35e4920cd88ba8d87b3b9fac05871158f4b5a
hvgazula <hvgazula@umich.edu>
2018-10-31 21:36:11 -0400

    Started writing for loop to handle multiple models and filters

--- local.py
+++ local.py
@@ -1,104 +1,50 @@
-#import glob
+import numpy as np
 import os
 import pandas as pd
+import regression as reg
 import sys
+import ujson as json
 from utils import listRecursive
 from patsy import dmatrix
-import regression as reg
-#import sys
-import ujson as json
-#import warnings
-#from parsers import fsl_parser
 from local_ancillary import local_stats_to_dict_fsl, ignore_nans
-import numpy as np
 
 
 def local_1(args):
     input_list = args["input"]
+
     covariates = input_list["covariates"]
     dependents = input_list["dependents"]
     roi = input_list["ROI"]
     models = input_list["models"]
     filters = input_list["filters"]
-    
-    curr_index = 0
 
     covariates_file = os.path.join(args["state"]["baseDirectory"], covariates)
-    y_file = os.path.join(args["state"]["baseDirectory"],
-                          'metr_' + dependents[curr_index] + '.csv')
-    
     X_main = pd.read_csv(covariates_file)
-    y_main = pd.read_csv(y_file)
-    y = y_main[roi[curr_index]]
+
+    for curr_model, curr_filter in zip(models, filters):
+        for curr_roi in roi:
+            for curr_dependent in dependents:
+                y_file = os.path.join(args["state"]["baseDirectory"],
+                                      'metr_' + curr_dependent + '.csv')
+
+                y_main = pd.read_csv(y_file)
+                y = y_main[curr_roi]
     
-    curr_model = models[curr_index]
-    curr_filter = filters[curr_index]
+                Xy = pd.concat([X_main, y], axis=1)
     
-    Xy = pd.concat([X_main, y], axis=1)
-
-    try:
-        Xy.query(curr_filter, inplace=True)
-    except ValueError:
-        pass
+                try:
+                    Xy.query(curr_filter, inplace=True)
+                except ValueError:
+                    pass
     
-    X = Xy.loc[:, Xy.columns != roi[curr_index]]
-    y = Xy.loc[:, Xy.columns == roi[curr_index]]
-
-    X = dmatrix(curr_model, data=X, return_type='dataframe')
-
-
-#    # Setting environment for the script to work seamlessly
-#    scriptDir = "/computation/enigma_scripts"
-#    resDir = os.path.join(args["state"]["outputDirectory"], 'results')
-#    logDir = os.path.join(args["state"]["outputDirectory"], 'logs')
-#
-#    data_dir = args["state"]["baseDirectory"]
-#    subjects_cov = os.path.join(args["state"]["baseDirectory"],
-#                                'covariates.csv')
-#    config_path = os.path.join(scriptDir, input_list["CONFIG_PATH"])
-#
-#    # Running the first script
-#    regr_args = [
-#        "bash",
-#        os.path.join(scriptDir, "mass_uv_regr_csv.sh"), scriptDir, resDir,
-#        logDir, data_dir, config_path, subjects_cov
-#    ]
-#    subprocess.call(regr_args)
-#
-#    # Running the second script
-#    concat_args = [
-#        "bash",
-#        os.path.join(scriptDir, "concat_mass_uv_regr_csv.sh"), scriptDir,
-#        resDir, logDir, data_dir, config_path
-#    ]
-#    subprocess.call(concat_args)
-#
-#    # Gather the list of ALL csv files
-#    agg_file_list = glob.glob(os.path.join(resDir, '*ALL*.csv'))
-
-    # Sending contents of the ALL files as dictionary with the file name being
-    # key and contents the value
-    #TODO: Can also read the files as dataframes which makes it easy to work on
-    # later
-#    output_contents = dict()
-#    for file in agg_file_list:
-#        file_name = os.path.split(file)[-1]
-#        file_name = os.path.splitext(file_name)[0]
-#
-#        with open(file, 'r') as f:
-#            lines = f.readlines()
-#
-#        output_contents[file_name] = lines
-#
-#    output_dict = {
-#        "output_contents": output_contents,
-#        "computation_phase": "local_1"
-#    }
-#
-
-    t = local_stats_to_dict_fsl(X, y)
-    beta_vector, local_stats_list, meanY_vector, lenY_vector = t
+                X = Xy.loc[:, Xy.columns != curr_roi]
+                y = Xy.loc[:, Xy.columns == curr_roi]
     
+                X = dmatrix(curr_model, data=X, return_type='dataframe')
+
+                t = local_stats_to_dict_fsl(X, y)
+                beta_vector, local_stats_list, meanY_vector, lenY_vector = t
+
     X_labels = list(X.columns)
     y_labels = list(y.columns)
     lamb = 0
@@ -160,7 +106,7 @@ def local_2(args):
 
     X = pd.read_json(cache_list["covariates"], orient='split')
     y = pd.read_json(cache_list["dependents"], orient='split')
-#    biased_X = sm.add_constant(X.values)
+    #    biased_X = sm.add_constant(X.values)
     biased_X = X.values
 
     avg_beta_vector = input_list["avg_beta_vector"]
@@ -188,7 +134,7 @@ def local_2(args):
     cache_dict = {}
 
     computation_output = {"output": output_dict, "cache": cache_dict}
-    
+
     return json.dumps(computation_output)
 
 
